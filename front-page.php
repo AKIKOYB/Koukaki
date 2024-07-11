d8b2103649f9d6c92dea37bbba8181b6b5f0e47e
AkikoYamamotoBeraud <akikoyamamotoberaud@gmail.com>
2024-07-11 21:11:10 +0200

    sections

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@ get_header();
 ?>
 
     <main id="primary" class="site-main">
+
         <section class="banner">
             <!-- adding logo -->
             <img class="banner__logo hidden" src="<?php echo  get_theme_file_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
@@ -14,14 +15,14 @@ get_header();
             </video>
         </section>
 
-        <section id="story" class="story">
+        <section id="story" class="story section_animate">
         <h2><span class="anim_title">L'histoire</span></h2>
             <article class="story__article">
                 <p class="anim_article article_animate"><?php echo get_theme_mod('story'); ?></p>
             </article>
         </section>
 
-        <section id="characters" class="characters anim_article article_animate">
+        <section id="characters" class="characters section_animate anim_article article_animate">
             <div>
                 <h3><span class="anim_title">Les personnages</span></h3>
             </div>
@@ -31,7 +32,7 @@ get_header();
             </div>
         </section>
 
-        <section id="place" class="place title_animate_1"><!-- must keep title_animate_1 for little cloud-->
+        <section id="place" class="place section_animate"><!-- must keep section_animate for little cloud-->
             <div class="cloud">
             <img src="<?php echo get_theme_file_uri() . '/assets/images/big_cloud.png'; ?>" class="cloud big_cloud" alt="Big Cloud">
             <img src="<?php echo get_theme_file_uri() . '/assets/images/little_cloud.png'; ?>" class="cloud little_cloud" alt="Little Cloud">
@@ -59,7 +60,7 @@ get_header();
                 const observer = new IntersectionObserver((entries, observer) => {
                     entries.forEach(entry => {
                         if (entry.isIntersecting) {
-                            entry.target.classList.add('title_animate_1');
+                            entry.target.classList.add('section_animate');
                             entry.target.classList.add('article_animate');
                             observer.unobserve(entry.target);
                         }
@@ -72,7 +73,7 @@ get_header();
             });
         </script>
 
-        <section id="studio" class="studio">
+        <section id="studio" class="studio section_animate">
             <h2><span class="anim_title slow">Studio</span> <span class="anim_title">Koukaki</span></h2>
             <div>
                 <p class="anim_article article_animate">Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
@@ -80,7 +81,7 @@ get_header();
             </div>
         </section>
 
-        <section id="oscars" class="oscars title_animate_1">
+        <section id="oscars" class="oscars section_animate">
             <h3><span class="anim_title">Fleurs d’oranger & chats errants</span><span>est nominé aux Oscars Short</span><span>Film Animated de 2022 !</span></h3>
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/oscars1.png" alt="Oscars">
         </section>
